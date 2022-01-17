14e1fceafa67e0f069dd967e234259e8b99b561f
mattv52 <matthewvolaris@yahoo.com.au>
2022-01-18 10:25:16 +1030

    Working on UI

--- 2D Game/Assets/Scripts/UI.cs
+++ 2D Game/Assets/Scripts/UI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
@@ -10,10 +11,24 @@ public class UI : MonoBehaviour
     public GameObject keysUI;
     // key sprites for render, 0=blue, 1=green, 2=red, 3=yellow
     public Sprite[] keys = new Sprite[4];
+    private int numKeys = 0;
 
+    private void Start()
+    {
+        addKey(2);
+        addKey(3);
+    }
     private void addKey(int key)
     {
-        SpriteRenderer sr = keysUI.AddComponent<SpriteRenderer>();
-        sr.sprite = keys[key];
+        // Create key sprite obj and add as a child
+        GameObject obj = new GameObject();
+        obj.transform.parent = keysUI.transform;
+
+        // Position in correct place
+        obj.transform.position = new Vector3(0f, -50f*numKeys);
+
+        // Add sprite to image component
+        Image img = obj.AddComponent<Image>();
+        img.sprite = keys[key];
     }
 }
